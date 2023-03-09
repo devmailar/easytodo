89c5c610c3776ec099a735d66aa7bd5785fbb3aa
Mailar <83369389+devmailar@users.noreply.github.com>
2023-03-09 20:28:48 +0200

    Add react-icons and react-hook-form dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { SiTask } from "react-icons/si";
+import "./App.css";
+import { Form } from "./components/Layout/Form";
+import { Header } from "./components/Layout/Header";
+import { Todos } from "./components/Todos/Todos";
+import { Task } from "./types";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const { register, handleSubmit } = useForm();
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    // Load tasks from local storage on mount
+    const savedTasks = localStorage.getItem("tasks");
+    if (savedTasks) {
+      console.log("Load tasks");
+      setTasks(JSON.parse(savedTasks));
+    }
+  }, []);
+
+  const onSubmit = (data: any) => {
+    const newTask: Task = { name: data.todoName, body: data.todoBody };
+    setTasks([...tasks, newTask]);
+
+    // Save tasks to local storage whenever a task is added
+    localStorage.setItem("tasks", JSON.stringify([...tasks, newTask]));
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="app">
+      <Header icon={<SiTask />} />
+      <Form register={register} onSubmit={handleSubmit(onSubmit)} />
+      <Todos tasks={tasks} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
