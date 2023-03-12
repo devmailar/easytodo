89fffb95e8f8a50715b8e47e10cadcf5d22ec53c
Mailar <83369389+devmailar@users.noreply.github.com>
2023-03-12 13:16:23 +0200

    Task notifications and delete functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { FiExternalLink } from "react-icons/fi";
 import { MdTaskAlt } from "react-icons/md";
+import { toast, ToastContainer } from "react-toastify";
 import "./App.css";
 import { CreateButton } from "./components/Layout/CreateButton";
 import { Form } from "./components/Layout/Form";
 import { Header } from "./components/Layout/Header";
 import { Todos } from "./components/Todos/Todos";
-
 import { Task } from "./types";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const { register, handleSubmit } = useForm();
@@ -54,6 +55,33 @@ function App() {
     localStorage.setItem("tasks", JSON.stringify([...tasks, newTask]));
   };
 
+  const handleEdit = (id: number, name: string) => {
+    console.log(`Edit clicked on ${id} ${name}`);
+  };
+
+  const handleComplete = (id: number, name: string) => {
+    console.log(`Complete clicked on ${id} ${name}`);
+  };
+
+  const handleDelete = (id: number, name: string) => {
+    console.log(`Delete clicked on ${id} ${name}`);
+
+    const updatedTasks = tasks.filter((task, index) => index !== id);
+
+    if (confirm(`Are you sure you want to delete todo: ${name}`)) {
+      setTasks(updatedTasks);
+      localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+
+      toast.error(`Deleted todo: ${name}!`, {
+        theme: "dark",
+        position: "top-center",
+        draggable: false,
+        pauseOnHover: false,
+        autoClose: 6000,
+      });
+    }
+  };
+
   return (
     <div className="app">
       <Header icon={<MdTaskAlt />} />
@@ -67,7 +95,25 @@ function App() {
           />
         )}
       </div>
-      <Todos tasks={tasks} />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <Todos
+        tasks={tasks}
+        handleEdit={handleEdit}
+        handleComplete={handleComplete}
+        handleDelete={handleDelete}
+      />
+      <ToastContainer />
     </div>
   );
 }
--- src/components/Todos/Todos.tsx
+++ src/components/Todos/Todos.tsx
@@ -7,38 +7,50 @@ import { BsPen } from "react-icons/bs";
 
 type TodosProps = {
   tasks: Task[];
+  handleEdit: (id: number, name: string) => void;
+  handleComplete: (id: number, name: string) => void;
+  handleDelete: (id: number, name: string) => void;
 };
 
-export const Todos: FC<TodosProps> = ({ tasks }) => (
-  <div className="todos">
-    {tasks.map((task: Task, index: number) => {
-      const limitedBody =
-        task.body.length > 200 ? `${task.body.slice(0, 200)}...` : task.body;
+export const Todos: FC<TodosProps> = ({
+  tasks,
+  handleEdit,
+  handleComplete,
+  handleDelete,
+}) => {
+  return (
+    <div className="todos">
+      {tasks.map((task: Task, index: number) => {
+        const limitedBody =
+          task.body.length > 200 ? `${task.body.slice(0, 200)}...` : task.body;
 
-      return (
-        <div key={index} className="task">
-          <div className="title-wrapper">
-            <h1 className="title">{task.name}</h1>
-            <div className="actions">
-              <BsPen />
-              <GiCheckMark />
-              <AiOutlineDelete />
+        return (
+          <div key={index} className="task">
+            <div className="title-wrapper">
+              <h1 className="title">{task.name}</h1>
+              <div className="actions">
+                <BsPen onClick={() => handleEdit(index, task.name)} />
+                <GiCheckMark onClick={() => handleComplete(index, task.name)} />
+                <AiOutlineDelete
+                  onClick={() => handleDelete(index, task.name)}
+                />
+              </div>
+            </div>
+
+            <div className="body-wrapper">
+              <p className="body">{limitedBody}</p>
+            </div>
+
+            <div className="footer-wrapper">
+              <p className="tag">{task.tag}</p>
+              <div className="date-wrapper">
+                <BsClockHistory />
+                <p className="date">{task.date}</p>
+              </div>
             </div>
           </div>
-
-          <div className="body-wrapper">
-            <p className="body">{limitedBody}</p>
-          </div>
-
-          <div className="footer-wrapper">
-            <p className="tag">{task.tag}</p>
-            <div className="date-wrapper">
-              <BsClockHistory />
-              <p className="date">{task.date}</p>
-            </div>
-          </div>
-        </div>
-      );
-    })}
-  </div>
-);
+        );
+      })}
+    </div>
+  );
+};
