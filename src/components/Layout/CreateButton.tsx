07ee0f3157b678e28ca8a4193a5e5b3e20d152a5
Mailar <83369389+devmailar@users.noreply.github.com>
2023-03-20 23:24:31 +0200

    Move icon to another side of button

--- src/components/Layout/CreateButton.tsx
+++ src/components/Layout/CreateButton.tsx
@@ -1,7 +1,7 @@
 export const CreateButton = ({ icon, handleOpen }: any) => (
   <div className="form-control">
     <button className="button" onClick={handleOpen}>
-      Add New {icon}
+      {icon}Add new
     </button>
   </div>
 );
