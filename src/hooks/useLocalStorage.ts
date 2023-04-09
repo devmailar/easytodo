858ff17bb1126f2fb85b2ba316b68fa7b15d507c
Mailar <83369389+devmailar@users.noreply.github.com>
2023-04-09 12:18:48 +0300

    Create useLocalStorage hook

---
+++ src/hooks/useLocalStorage.ts
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+
+export const useLocalStorage = <T>(key: string, initialValue: T) => {
+  const [value, setValue] = useState(() => {
+    const jsonValue = window.localStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : initialValue;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+};
