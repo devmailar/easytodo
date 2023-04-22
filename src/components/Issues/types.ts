b88d96de762608f6f323042f9e550831d5b4e607
Mailar <83369389+devmailar@users.noreply.github.com>
2023-04-22 09:45:19 +0300

    Format components issues and list

--- src/components/Issues/types.ts
+++ src/components/Issues/types.ts
@@ -1,18 +1,18 @@
-import { TIssue } from "../../types";
+import { TIssue } from '../../types';
 
 export type THeader = {
-  name: string;
+    name: string;
 };
 
 export type TBody = {
-  description: string;
+    description: string;
 };
 
 export type TFooter = {
-  index: number;
-  color: string;
-  issue: TIssue;
-  handleEdit: (id: number, name: string, description: string, priority: string) => void;
-  handleComplete: (id: number, name: string) => void;
-  handleDelete: (id: number, name: string, createdDate: string) => void;
+    index: number;
+    color: string;
+    issue: TIssue;
+    handleEdit: (id: number, name: string, description: string, priority: string) => void;
+    handleComplete: (id: number, name: string) => void;
+    handleDelete: (id: number, name: string, createdDate: string) => void;
 };
