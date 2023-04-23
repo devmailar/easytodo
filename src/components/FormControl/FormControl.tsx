9bbd8f55125bb3c06be1aa4bb6817a362200c97f
Mailar <83369389+devmailar@users.noreply.github.com>
2023-04-23 20:54:04 +0300

    Remove FormControl component

--- src/components/FormControl/FormControl.tsx
+++
@@ -1,8 +0,0 @@
-import './FormControl.css';
-
-export const FormControl = ({ addIcon, filterIcon, handleAddClick, handleFilterClick }: any) => (
-  <div className="app-form-control">
-    <button onClick={handleAddClick}>{addIcon}Add new</button>
-    <button onClick={handleFilterClick}>{filterIcon}Filter</button>
-  </div>
-);
