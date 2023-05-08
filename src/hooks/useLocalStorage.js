77e3c25c193f5d0477285d87124084998245e1e6
Mailar <83369389+devmailar@users.noreply.github.com>
2023-05-08 15:58:57 +0300

    Remove typescript version of hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+export function useLocalStorage(key, initialValue) {
+  const [value, setValue] = useState(() => {
+    const storedValue = window.localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialValue;
+  });
+
+  useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+
+  return [value, setValue];
+}
