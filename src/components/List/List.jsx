1acff704df84e2b6b7bd92a37a7dc112380ad39e
Mailar <83369389+devmailar@users.noreply.github.com>
2023-05-13 16:39:22 +0300

    Update list

---
+++ src/components/List/List.jsx
@@ -0,0 +1,7 @@
+import styles from "./styles/List.module.css";
+
+const List = ({ children }) => {
+  return <ul className={styles.list}>{children}</ul>;
+};
+
+export default List;
--- src/components/List/List.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-import styles from './List.module.css';
-import { TList } from './types';
-
-const List: FC<TList> = ({ children }) => {
-    return <ul className={styles.list}>{children}</ul>;
-};
-
-export default List;
