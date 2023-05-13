93123ac179fece48d4abfce3bfa6d21bff5abca3
Mailar <83369389+devmailar@users.noreply.github.com>
2023-05-13 16:41:41 +0300

    Update dependencies and use jsx in html

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@Components": path.resolve(__dirname, "./src/components"),
+      "@Pages": path.resolve(__dirname, "./src/pages"),
+      "@Hooks": path.resolve(__dirname, "./src/hooks"),
+      "@Utils": path.resolve(__dirname, "./src/utils"),
+    },
+  },
+});
